b8baef121690a5270314fecf53cb0a7b9c43c8b2
iamluhmrt <luizhenrique.business95@gmail.com>
2026-05-25 20:45:51 -0300

    feat: add binary search

--- binary-search/binary_search.py
+++ binary-search/binary_search.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+def binary_search(list, item):
+  down = 0;
+  high = len(list) - 1;
+
+  while down <= high:
+    middle = (down + high) // 2;
+    shot = list[middle];
+    if shot == item:
+      return middle;
+    if shot > item:
+      high = middle - 1;
+    else:
+      down = middle + 1;
+  return None;
+
+
+students_ages = [11,12,13,14,15,16,17,18];
+
+print(binary_search(students_ages, 13));
